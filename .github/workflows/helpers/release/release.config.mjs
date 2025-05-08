e6e75d5bdb75deb79371c7c6bc82172d3a830013
Muhammad Rendra <mnrendra@users.noreply.github.com>
2025-05-09 02:32:14 +0700

    feat: susi

---
+++ .github/workflows/helpers/release/release.config.mjs
@@ -0,0 +1,37 @@
+const branches = [
+  '+([0-9])?(.{+([0-9]),x}).x',
+  'main',
+  'next',
+  { name: 'rc', prerelease: true },
+  { name: 'beta', prerelease: true },
+  { name: 'alpha', prerelease: true }
+]
+
+const plugins = [
+  ['@semantic-release/commit-analyzer', {
+    releaseRules: [
+      { type: 'docs', release: 'patch' },
+      { type: 'style', release: 'patch' },
+      { type: 'refactor', release: 'patch' },
+      { type: 'perf', release: 'patch' },
+      { type: 'test', release: 'patch' },
+      { type: 'build', release: 'patch' },
+      { type: 'ci', release: 'patch' },
+      { type: 'chore', release: 'patch' },
+      { type: 'revert', release: 'patch' }
+    ]
+  }],
+  '@semantic-release/release-notes-generator',
+  ['@semantic-release/exec', {
+    prepareCmd: 'echo "prepare: ${nextRelease.version}"',
+    publishCmd: 'echo "publish: ${nextRelease.version}"',
+    successCmd: 'echo "success: ${nextRelease.version}"'
+  }]
+]
+
+const config = {
+  branches,
+  plugins
+}
+
+export default config
