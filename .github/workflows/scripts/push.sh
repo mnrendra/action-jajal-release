ca423f83b70593c88234fdcfc11269753bd8500d
Muhammad Rendra <mnrendra@users.noreply.github.com>
2025-05-17 16:15:27 +0700

    feat: fskjdhfs

--- .github/workflows/scripts/push.sh
+++ .github/workflows/scripts/push.sh
@@ -7,6 +7,10 @@ push() {
   local tag="${3:-""}"
 
   git config --list
+  echo "GIT_AUTHOR_NAME:" "$GIT_AUTHOR_NAME"
+  echo "GIT_AUTHOR_EMAIL:" "$GIT_AUTHOR_EMAIL"
+  echo "GIT_COMMITTER_NAME:" "$GIT_COMMITTER_NAME"
+  echo "GIT_COMMITTER_EMAIL:" "$GIT_COMMITTER_EMAIL"
 
   git commit -S --allow-empty -m "$message"
   git push origin "$branch"
