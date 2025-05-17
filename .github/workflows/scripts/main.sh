8bd35e84ce798a1b1804c7a517ab29c2a948390a
GitOps Release <gitops-release@users.noreply.github.com>
2025-05-17 17:21:21 +0000

    latest: v8.35.0

---
+++ .github/workflows/scripts/main.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -euo pipefail
+
+source "$(dirname "$0")/consts.sh"
+source "$(dirname "$0")/parse-ignore.sh"
+source "$(dirname "$0")/backup-gitignore.sh"
+source "$(dirname "$0")/generate-gitignore.sh"
+source "$(dirname "$0")/update-action-version.sh"
+source "$(dirname "$0")/git-add.sh"
+source "$(dirname "$0")/push.sh"
+source "$(dirname "$0")/restore-gitignore.sh"
+source "$(dirname "$0")/unstage-ghaignore.sh"
+
+main() {
+  local version="$1"
+  local notes="${2:-""}"
+  local branch="${3:-"$BRANCH"}"
+  local action_file="${4:-"$ACTION_FILE"}"
+  local tag="${5:-"v$version"}"
+  local release_message="${6:-"release: "$tag""$'\n\n'"$notes"}"
+  local latest_message="${7:-"latest: "$tag""}"
+  local parsed_gha_ignore
+  local parsed_git_ignore
+
+  if [ -n "${version:-}" ] && [ -n "${branch:-}" ]; then
+    parsed_gha_ignore="$(parse_ignore "$GHA_IGNORE_FILE")"
+    parsed_git_ignore="$(parse_ignore "$GIT_IGNORE_FILE")"
+
+    backup_gitignore
+
+    generate_gitignore "$parsed_gha_ignore"
+
+    update_action_version "$action_file" "$version"
+
+    git_add "$parsed_git_ignore" "$parsed_gha_ignore"
+
+    push "$branch" "$release_message" "$tag"
+
+    restore_gitignore
+
+    unstage_ghaignore "$parsed_git_ignore" "$parsed_gha_ignore"
+
+    git_add "$parsed_gha_ignore" "$parsed_git_ignore"
+
+    push "$branch" "$latest_message"
+  fi
+}
+
+main "$1" "${2:-""}" "${3:-""}" "${4:-""}" "${5:-""}" "${6:-""}" "${7:-""}"
